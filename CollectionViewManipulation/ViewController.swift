4bd955696e3940eb84fc271f1ed71118f2411698
Henry Heguang Miao <heguang.miao@coroma.com.au>
2018-06-16 11:35:19 +1000

    Initial Commit

---
+++ CollectionViewManipulation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewManipulation
+//
+//  Created by Henry Miao on 16/6/18.
+//  Copyright © 2018 Henry Miao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
